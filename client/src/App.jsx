78f79c9372d17b9f6a74e837fdd2e4c2917b6940
Kunal Nandi <akakunal2903@gmail.com>
2026-03-24 21:23:42 +0530

    created login page succesfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
+import ChatBox from "./pages/ChatBox";
+import Connections from "./pages/Connections";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
+import Layout from "./pages/Layout";
+import { useUser } from "@clerk/react";
 
 const App = () => {
-  return <div>Hello world</div>;
+  const { user } = useUser();
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={!user ? <Login /> : <Layout />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
